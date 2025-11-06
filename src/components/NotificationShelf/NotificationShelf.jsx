ac2d2754d7a1a40c728fb0539a3cf59e5896f599
jraczynski <raczynski.joseph616@gmail.com$ git config --global user.email raczynski.joseph616@gmail.com>
2025-11-06 12:18:35 -0600

    Add NotificationShelf.jsx

---
+++ src/components/NotificationShelf/NotificationShelf.jsx
@@ -0,0 +1,14 @@
+export default function NotificationShelf() {
+    return (
+        <div className="notifications-shelf">
+            <div className="shelf-header">
+                <h3>Notifications</h3>
+                <button className="close-shelf-btn">X</button>
+            </div>
+
+            <div className="shelf-content">
+                {/* TODO: Create notification card component */}
+            </div>
+        </div>
+    )
+}

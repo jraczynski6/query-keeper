93ad1d82c79cc52b9af3dbcd68e7286477ab2b45
jraczynski <raczynski.joseph616@gmail.com$ git config --global user.email raczynski.joseph616@gmail.com>
2025-11-06 01:18:53 -0600

    Add book info form to GenerateQueryModal.jsx

--- src/components/modals/GenerateQueryModal.jsx
+++ src/components/modals/GenerateQueryModal.jsx
@@ -5,6 +5,7 @@ export default function GenerateQueryModal() {
                 <h2>Generate a Query</h2>
                 <button className="modal-close">X</button>
                 <form className="generate-query-form">
+                    {/* Author selection */}
                     <label>
                         Author:
                         <select>
@@ -12,6 +13,7 @@ export default function GenerateQueryModal() {
                         </select>
                     </label>
 
+                    {/* Agent Selection */}
                     <label>
                         Agent:
                         <select>
@@ -19,12 +21,73 @@ export default function GenerateQueryModal() {
                         </select>
                     </label>
 
+                    {/* Query Template */}
                     <label>
                         Query Template:
                         <select>
                             <option>Select a template</option>
                         </select>
                     </label>
+
+                    {/* Book Info Section */}
+                    <fieldset>
+                        <legend>Books Info</legend>
+
+                        <label>
+                            Title:
+                            <input type="text" placeholder="Enter book title" required />
+                        </label>
+
+                        <label>
+                            Word Count:
+                            <input type="number" placeholder="Enter word count" required/>
+                        </label>
+
+                        <label>
+                            Genre:
+                            <select defaultValue={""}>
+                                <option value={"nonfiction"}>Nonfiction</option>
+                                <option value={"fantasy"}>Fantasy</option>
+                                <option value={"Sci-fi"}>Sci-fi</option>
+                                <option value={"mystery"}>Mystery</option>
+                                <option value={"horror"}>Horror</option>
+                                <option value={"dystopian"}>Dystopian</option>
+                                <option value={"nonfiction"}>Nonfiction</option>
+                                <option value={"literaryfiction"}>Literary Fiction</option>
+                                <option value={"thriller"}>Thriller</option>
+                                <option value={"other"}>Other</option>
+                            </select>
+                        </label>
+
+                        {/* TODO: Add handleGenreChange logic and custom genre input */}
+                        <label id="customGenreLabel" style={{display: "none"}}>
+                            Enter Genre:
+                            <input type="text" id="customGenre" placeholder="Enter genre" />
+                        </label>
+                    </fieldset>
+
+                    {/* Sample Size Section */}
+                    <fieldset>
+                        <legend>Sample Size</legend>
+
+                        <label>
+                            Select sample size:
+                            <select>
+                                <option value={""}>Select...</option>
+                                <option value={"3"}>3 Pages</option>
+                                <option value={"5"}>5 Pages</option>
+                                <option value={"10"}>10 Pages</option>
+                                <option value={"30"}>30 Pages</option>
+                                <option value={"50"}>50 Pages</option>
+                            </select>
+                        </label>
+
+                        {/* TODO: Show text area only when size selected */}
+                        <label>
+                            Enter text for selected page size:
+                            <textarea placeholder="Enter text for selected pages" />
+                        </label>
+                    </fieldset>
 
                     <button type="submit">Generate</button>
                     <button type="button">Cancel</button>
